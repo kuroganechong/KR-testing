02bc7baa0da63d95da93c293414443de86791a62
kuroganechong <kuroganechong@gmail.com>
2018-12-14 12:45:58 +0800

    modified data

---
+++ data/English/hero/calc.py
@@ -0,0 +1,8 @@
+while(1):
+    x = int(input("base value: "))
+    print(round(x * 0.2))
+    print(round(x * 0.24))
+    print(round(x * 0.29))
+    print(round(x * 0.35))
+    print(round(x * 0.42))
+    print(round(x * 0.50))
